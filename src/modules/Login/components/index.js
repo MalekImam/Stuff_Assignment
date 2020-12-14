ad2aabc4314338b438e0e3f8be49f346c357305f
Malek <malekalemam20@gmail.com>
2020-12-14 15:47:20 +0200

    Hide App Bar From Login Screen

--- src/modules/Login/components/index.js
+++ src/modules/Login/components/index.js
@@ -2,8 +2,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Text, View} from 'react-native';
+// Shared Components
+import NavBar from 'shared/layouts/NavBar';
 
 function Login({navigation}) {
+  // Remove navigation header from the screen
+  React.useLayoutEffect(() => {
+    navigation.setOptions({headerShown: false});
+  }, [navigation]);
+
   return (
     <View>
       <Text>Login Page</Text>
