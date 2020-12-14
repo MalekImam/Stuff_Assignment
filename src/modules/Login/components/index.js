7d874d9b72fbf70b833209f4aed4ec177b861555
Malek <malekalemam20@gmail.com>
2020-12-14 15:48:09 +0200

    remove un-used links from login component

--- src/modules/Login/components/index.js
+++ src/modules/Login/components/index.js
@@ -2,8 +2,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Text, View} from 'react-native';
-// Shared Components
-import NavBar from 'shared/layouts/NavBar';
 
 function Login({navigation}) {
   // Remove navigation header from the screen
