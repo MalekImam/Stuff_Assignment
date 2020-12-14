0d96aa61e8d32996ac86265a1e8e1b1dcd4fc583
Malek <malekalemam20@gmail.com>
2020-12-14 15:41:57 +0200

    Wrap the project with the paper ui provider

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import {enableScreens} from 'react-native-screens';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {Provider as PaperProvider} from 'react-native-paper';
 // Core Files
 import useSelect from 'core/hooks/useSelect';
 import {authRoutesList} from 'core/Routes/router';
@@ -15,29 +16,31 @@ const App = () => {
   const user = useSelect('userReducer.user');
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        {user
-          ? authRoutesList.map((route, index) => {
-              return (
-                <Stack.Screen
-                  key={index}
-                  name={route.name}
-                  component={route.component}
-                />
-              );
-            })
-          : unAuthRoutesList.map((route, index) => {
-              return (
-                <Stack.Screen
-                  key={index}
-                  name={route.name}
-                  component={route.component}
-                />
-              );
-            })}
-      </Stack.Navigator>
-    </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          {user
+            ? authRoutesList.map((route, index) => {
+                return (
+                  <Stack.Screen
+                    key={index}
+                    name={route.name}
+                    component={route.component}
+                  />
+                );
+              })
+            : unAuthRoutesList.map((route, index) => {
+                return (
+                  <Stack.Screen
+                    key={index}
+                    name={route.name}
+                    component={route.component}
+                  />
+                );
+              })}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 };
 
