b29eb52f08091e1f99e1ee3d406dd698b63b64f2
Malek <malekalemam20@gmail.com>
2020-12-16 21:32:42 +0200

    Clear Redux Moment state when navigating to date or time screen

--- src/modules/Home/components/BodyButton.js
+++ src/modules/Home/components/BodyButton.js
@@ -1,14 +1,21 @@
 // Dependencies
 import React from 'react';
 import PropTypes from 'prop-types';
+import {useDispatch} from 'react-redux';
 import {useNavigation} from '@react-navigation/native';
+// Core Files
+import {clearMoment} from 'core/redux/actions/moment';
 // Shared Components
 import homeStyles from 'modules/Home/styles';
 import StaticButton from 'shared/components/Buttons/StaticButton';
 
 const BodyButton = ({label}) => {
+  const dispatch = useDispatch();
   const navigation = useNavigation();
-  const navigateTo = (target) => navigation.navigate(target);
+  const navigateTo = (target) => {
+    dispatch(clearMoment());
+    navigation.navigate(target);
+  };
   return (
     <StaticButton
       label={label}
