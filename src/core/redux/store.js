df9bf0c8480426f3bedc4ea8521e372e92453064
Malek <malekalemam20@gmail.com>
2020-12-17 13:08:01 +0200

    Add firebase & firestore reducers to the redux root reducer

--- src/core/redux/store.js
+++ src/core/redux/store.js
@@ -1,12 +1,22 @@
 // Dependencies
 import thunk from 'redux-thunk';
-import {reactReduxFirebase, getFirebase} from 'react-redux-firebase';
 import fbConfig from 'core/config/firebaseConfig';
 import userReducer from 'core/redux/reducers/userReducer';
-import {reduxFirestore, getFirestore} from 'redux-firestore';
 import momentReducer from 'core/redux/reducers/momentReducer';
 import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
-const rootReducers = combineReducers({userReducer, momentReducer});
+import {reduxFirestore, firestoreReducer, getFirestore} from 'redux-firestore';
+import {
+  getFirebase,
+  firebaseReducer,
+  reactReduxFirebase,
+} from 'react-redux-firebase';
+
+const rootReducers = combineReducers({
+  userReducer,
+  momentReducer,
+  firebaseReducer,
+  firestoreReducer,
+});
 
 const configureStore = () =>
   createStore(
