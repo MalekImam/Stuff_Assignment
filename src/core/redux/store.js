2f82b2b768f39e6fdc93d7dcd7b5e44e99376c17
Malek <malekalemam20@gmail.com>
2020-12-17 06:27:35 +0200

    Apply Middleware to redux store

--- src/core/redux/store.js
+++ src/core/redux/store.js
@@ -1,9 +1,11 @@
-import {createStore, combineReducers} from 'redux';
+// Dependencies
+import thunk from 'redux-thunk';
 import userReducer from 'core/redux/reducers/userReducer';
 import momentReducer from 'core/redux/reducers/momentReducer';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
 
 const rootReducers = combineReducers({userReducer, momentReducer});
 
-const configureStore = () => createStore(rootReducers);
+const configureStore = () => createStore(rootReducers, applyMiddleware(thunk));
 
 export default configureStore;
