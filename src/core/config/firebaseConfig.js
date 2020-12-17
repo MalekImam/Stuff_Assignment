45a597dcf6cd647143116661946427dd32c96654
Malek <malekalemam20@gmail.com>
2020-12-17 07:20:40 +0200

    Change Firebase configs

--- src/core/config/firebaseConfig.js
+++ src/core/config/firebaseConfig.js
@@ -4,13 +4,12 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 var firebaseConfig = {
-  apiKey: 'AIzaSyAEw2FdW6DXEfGAXSBY7LMyg9diKPsPDjQ',
-  authDomain: 'stuff-assignment.firebaseapp.com',
-  projectId: 'stuff-assignment',
-  storageBucket: 'stuff-assignment.appspot.com',
-  messagingSenderId: '523689283503',
-  appId: '1:523689283503:web:00d4d20737b57bfbfae95a',
-  measurementId: 'G-2D639YLZ5B',
+  apiKey: 'AIzaSyCF1OH8ecQF4EzkK3T9QSYeE-Ke6x8GC3M',
+  authDomain: 'stuffapp-ebfe7.firebaseapp.com',
+  projectId: 'stuffapp-ebfe7',
+  storageBucket: 'stuffapp-ebfe7.appspot.com',
+  messagingSenderId: '837951499462',
+  appId: '1:837951499462:web:0db2bf91bb16aa43243bc7',
 };
 
 // Initialize Firebase
